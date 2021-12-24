732c3606fcfc3632fbba75d4be19b207df71ea44
salma122110 <66427066+salma122110@users.noreply.github.com>
2021-12-24 02:23:53 +0200

    Update Consistency.java

--- Consistency.java
+++ Consistency.java
@@ -8,24 +8,27 @@
 
        for(int i=0;i<lines.length;i++)
        {  
+        // deleting the spaces at the end and the begining of the lines of the array
         lines[i]=lines[i].trim();
               
         String str1 = lines[i].substring(0,lines[i].length());
-     //   System.out.println(str1);
+    
      int startOpenning = lines[i].indexOf("<");
      int endStarting = lines[i].indexOf(">"); 
            boolean x = str1.contains(" ");
-           boolean spaces = lines[i].indexOf(" ")< lines [i].indexOf ("</");
-           if( lines[i].contains("<")  && ( (lines[i].charAt(startOpenning+1) == '!') || (lines[i].charAt(startOpenning+1) == '?')  )   )
+           boolean spaces = lines[i].indexOf(" ") < lines [i].indexOf ("</");
+          // skip the lines containing <? or <! 
+           if( lines[i].contains("<")  && ( (lines[i].charAt(startOpenning+1) == '!') || (lines[i].charAt(startOpenning+1) == '?')  ) || lines[i].trim().length() == 0    )
            {
             
                continue;
            }
         
-                   // openning
+                   // checking the opening tag
                    
             if( lines[i].contains("<")  && (lines[i].charAt(startOpenning+1) != '/')  )
-            {       
+            {      
+                   // pushing the tag name in the stack
                  if( !x )
                  {
                     str = lines[i].substring(startOpenning+1,endStarting);
@@ -38,7 +41,7 @@
                    
                  }
 
-            
+               // check if the line contains spaces which means containing attribute
                 else 
                 {
                 str = lines[i].substring(startOpenning+1,lines[i].indexOf(" "));
@@ -47,7 +50,7 @@
                
                 }
             }
-            //  closing
+            //  checking the closing tag
             int startClosing= lines[i].indexOf("/");
             int endClosing=lines[i].length()-1;
             
@@ -62,17 +65,14 @@
                        
                         stk2.pop();
                        
-
                     }
                     else
                     {
                        
-                            cons="Error in consistency";
-                       
-                        
-                    
-                }}
-                
+                            cons="Error in consistency";        
+                }
+            }
+                // no error
             if (stk.empty())
             {
                     cons="File is consistent";
